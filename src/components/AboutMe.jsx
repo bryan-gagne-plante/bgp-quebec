122efdbffce2fa31feab1ab4875cc1307bce1b1e
Bryan <1962402@csfoy.ca>
2024-03-09 20:40:09 -0500

    Ajout du composant React AboutMe

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const AboutMe = () => {
+    const { t } = useTranslation();
+
+    return (
+        <>
+            <h1>{t('AboutMe-Title')}</h1>
+            <span>{t('AboutMe-Subtitle')}</span>
+            <ul>
+                <li>{t('AboutMe-Line1')}</li>
+                <li>{t('AboutMe-Line2')}</li>
+                <li>{t('AboutMe-Line3')}</li>
+                <li>{t('AboutMe-Line4')}</li>
+                <li>{t('AboutMe-Line5')}</li>
+                <li>{t('AboutMe-Line6')}</li>
+            </ul>
+            <p>Arrête de chercher, tu viens de le trouver !</p>
+        </>
+    );
+}
+
+export default AboutMe;
