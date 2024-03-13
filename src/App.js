870a97db2d58806fc22f342597fd7cbad214f7e6
Bryan <1962402@csfoy.ca>
2024-03-13 14:30:46 -0400

    Deplacement du style vers les composants

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
+import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import {AboutMe} from './components/AboutMe';
+
+import AboutMe from './components/AboutMe';
 import Langages from './components/Langages';
-import {Col, Container, Row} from "react-bootstrap";
 import ThemeSwitch from "./components/ThemeSwitch";
+
 
 function App() {
     return (
-        <Container fluid>
-            <Row>
-                <Col>
-                    <ThemeSwitch/>
-                </Col>
-                <Col>
-                    <Langages/>
-                </Col>
-            </Row>
+        <Container>
+            <Langages/>
             <AboutMe/>
+            <ThemeSwitch/>
         </Container>
     );
 }
